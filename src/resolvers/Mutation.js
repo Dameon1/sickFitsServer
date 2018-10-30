e2a46097faa828c73c4956abd533a127fab3d235
Dameon1 <dameonmedoza@gmail.com>
2018-10-30 09:54:07 -0700

    updating permissions on the server

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { transport, makeANiceEmail } = require('../mail');
-
+const { hasPermission } = require('../utils');
 
 
 const Mutations = {
@@ -146,9 +146,31 @@ const Mutations = {
       maxAge: 1000* 60 * 60 * 24 * 365,
     });
     return updatedUser;
-  }
-
-
+  },
+  async updatePermissions(parent, args, ctx, info){
+    if(!ctx.request.userId){
+      throw new Error('You must be logged in to perform this action')
+    };
+    const currentUser = await ctx.db.query.user(
+      {
+        where: {id: ctx.request.userId, 
+        },
+      }, 
+      info
+    );
+    hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']
+    );
+    return ctx.db.mutation.updateUser({
+      data: {
+        permissions: {
+          set: args.permissions,
+        }
+      },
+      where: {
+        id: args.userId,
+      },
+    }, info);
+  },
 };
 
 module.exports = Mutations;
